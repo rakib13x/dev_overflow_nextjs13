2975886d37f0b90689e50f6abe046b8dbaf28778
rakib13x <rakib13x@gmail.com>
2023-12-13 23:17:56 +0600

    Navbar_with_dark_light_mode_functionality_implemented

--- context/ThemeProvider.tsx
+++ context/ThemeProvider.tsx
@@ -11,18 +11,24 @@ const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
   const [mode, setMode] = useState("");
   const handleThemeChange = () => {
-    if (mode === "dark") {
-      setMode("light");
-      document.documentElement.classList.add("light");
-    } else {
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme:dark)").matches)
+    ) {
       setMode("dark");
       document.documentElement.classList.add("dark");
+    } else {
+      setMode("light");
+      document.documentElement.classList.remove("dark");
     }
   };
 
   useEffect(() => {
     handleThemeChange();
   }, [mode]);
+
+  console.log("current mode is:", mode);
 
   return (
     <ThemeContext.Provider value={{ mode, setMode }}>
