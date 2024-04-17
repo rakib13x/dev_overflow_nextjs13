0cebd6db68cd31e9415bf17e86cb721a74e2c010
Rakib Islam <rakib71xx@gmail.com>
2024-04-17 15:59:14 +0600

    upvote image with design added

--- components/Shared/Votes.tsx
+++ components/Shared/Votes.tsx
@@ -1,5 +1,45 @@
-const Votes = () => {
-  return <div>Votes</div>;
+import Image from "next/image";
+
+interface Props {
+  type: string;
+  itemId: string;
+  userId: string;
+  upvotes: number;
+  hasupVoted: boolean;
+  downvotes: number;
+  hasdownVoted: boolean;
+  hasSaved?: boolean;
+}
+
+const Votes = ({
+  type,
+  itemId,
+  userId,
+  upvotes,
+  hasupVoted,
+  downvotes,
+  hasdownVoted,
+  hasSaved,
+}: Props) => {
+  return (
+    <div className="flex gap-5">
+      <div className="flex-center gap-2.5">
+        <div className="flex-center gap-1.5">
+          <Image
+            src={
+              hasupVoted
+                ? "/assets/icons/upvoted.svg"
+                : "/assets/icons/upvote.svg"
+            }
+            width={18}
+            height={18}
+            alt="upvote"
+            className="cursor-pointer"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Votes;
