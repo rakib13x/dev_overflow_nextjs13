eb145d9fc19e9e0911e1b10a6ababd747987f280
Rakib Islam <rakib71xx@gmail.com>
2024-04-17 12:00:37 +0600

    create answer submission  added after publishing new branch

--- lib/actions/answer.action.ts
+++ lib/actions/answer.action.ts
@@ -6,7 +6,7 @@ import Question from "@/database/question.model";
 import User from "@/database/user.model";
 import { revalidatePath } from "next/cache";
 import { connectToDatabase } from "../mongoose";
-import { CreateAnswerParams, GetAnswersParams } from "./shared.types";
+import { CreateAnswerParams } from "./shared.types";
 
 export async function createAnswer(params: CreateAnswerParams) {
   try {
@@ -31,32 +31,6 @@ export async function createAnswer(params: CreateAnswerParams) {
     await User.findByIdAndUpdate(author, { $inc: { reputation: 10 } });
 
     revalidatePath(path);
-  } catch (error) {
-    console.log(error);
-    throw error;
-  }
-}
-
-export async function getAnswers(params: GetAnswersParams) {
-  try {
-    connectToDatabase();
-
-    const { questionId, sortBy, page = 1, pageSize = 10 } = params;
-
-    const skipAmount = (page - 1) * pageSize;
-
-    const answers = await Answer.find({ question: questionId })
-      .populate("author", "_id clerkId name picture")
-      .skip(skipAmount)
-      .limit(pageSize);
-
-    const totalAnswer = await Answer.countDocuments({
-      question: questionId,
-    });
-
-    const isNextAnswer = totalAnswer > skipAmount + answers.length;
-
-    return { answers, isNextAnswer };
   } catch (error) {
     console.log(error);
     throw error;
