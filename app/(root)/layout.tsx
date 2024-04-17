7eda038f3bfca963b05400315b152c3423405032
Rakib Islam <rakib71xx@gmail.com>
2024-04-17 16:28:37 +0600

    toaster added

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,7 @@
 import LeftSidebar from "@/components/Shared/LeftSidebar";
 import Navbar from "@/components/Shared/Navbar/Navbar";
 import RightSideBar from "@/components/Shared/RightSideBar";
+import { Toaster } from "@/components/ui/toaster";
 import React from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
@@ -18,7 +19,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
         </section>
         <RightSideBar />
       </div>
-      Toaster
+      <Toaster />
     </main>
   );
 };
