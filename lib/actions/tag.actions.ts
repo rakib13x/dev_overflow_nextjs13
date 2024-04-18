f5e81cb6be6fa77ded5b1521046cfe950bae5a04
Rakib Islam <rakib71xx@gmail.com>
2024-04-18 10:27:10 +0600

    getQuestionsByTagId action added

--- lib/actions/tag.actions.ts
+++ lib/actions/tag.actions.ts
@@ -1,10 +1,15 @@
 "use server";
 
-import Tag from "@/database/tag.model";
+import Question from "@/database/question.model";
+import Tag, { ITag } from "@/database/tag.model";
 import User from "@/database/user.model";
 import { FilterQuery } from "mongoose";
 import { connectToDatabase } from "../mongoose";
-import { GetAllTagsParams, GetTopInteractedTagsParams } from "./shared.types";
+import {
+  GetAllTagsParams,
+  GetQuestionsByTagIdParams,
+  GetTopInteractedTagsParams,
+} from "./shared.types";
 
 export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
   try {
@@ -78,4 +83,43 @@ export async function getAllTags(params: GetAllTagsParams) {
   }
 }
 
-export async function getQuestionsByTagId() {}
+export async function getQuestionsByTagId(params: GetQuestionsByTagIdParams) {
+  try {
+    connectToDatabase();
+
+    const { tagId, page = 1, pageSize = 10, searchQuery } = params;
+    const skipAmount = (page - 1) * pageSize;
+
+    const tagFilter: FilterQuery<ITag> = { _id: tagId };
+
+    const tag = await Tag.findOne(tagFilter).populate({
+      path: "questions",
+      model: Question,
+      match: searchQuery
+        ? { title: { $regex: searchQuery, $options: "i" } }
+        : {},
+      options: {
+        sort: { createdAt: -1 },
+        skip: skipAmount,
+        limit: pageSize + 1, // +1 to check if there is next page
+      },
+      populate: [
+        { path: "tags", model: Tag, select: "_id name" },
+        { path: "author", model: User, select: "_id clerkId name picture" },
+      ],
+    });
+
+    if (!tag) {
+      throw new Error("Tag not found");
+    }
+
+    const isNext = tag.questions.length > pageSize;
+
+    const questions = tag.questions;
+
+    return { tagTitle: tag.name, questions, isNext };
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
