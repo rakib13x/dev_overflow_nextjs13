50f7bf5355adc05c7790e47724699cb94d609846
Rakib Islam <rakib71xx@gmail.com>
2024-04-18 14:59:09 +0600

    updated LeftSideBar component for showing profile details

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,11 +1,62 @@
+import { Button } from "@/components/ui/button";
 import { getUserInfo } from "@/lib/actions/user.action";
 import { URLProps } from "@/types";
-import { auth } from "@clerk/nextjs";
+import { SignedIn, auth } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
 
-const page = async ({ params, searchParams }: URLProps) => {
+const Page = async ({ params, searchParams }: URLProps) => {
   const { userId: clerkId } = auth();
   const userInfo = await getUserInfo({ userId: params.id });
-  return <div>page</div>;
+
+  return (
+    <>
+      <div className="flex flex-col-reverse items-start justify-between sm:flex-row">
+        <div className="flex flex-col items-start gap-4 lg:flex-row">
+          <Image
+            src={userInfo?.user.picture}
+            alt="profile picture"
+            width={140}
+            height={140}
+            className="rounded-full object-cover"
+          />
+
+          <div className="mt-3">
+            <h2 className="h2-bold text-dark100_light900">
+              {userInfo.user.name}
+            </h2>
+            <p className="paragraph-regular text-dark200_light800">
+              @{userInfo.user.username}
+            </p>
+
+            <div className="mt-5 flex flex-wrap items-center justify-start gap-5">
+              {userInfo.user.portfolioWebsite}
+
+              {userInfo.user.location}
+            </div>
+
+            {userInfo.user.bio && (
+              <p className="paragraph-regular text-dark400_light800 mt-8">
+                {userInfo.user.bio}
+              </p>
+            )}
+          </div>
+        </div>
+
+        <div className="flex justify-end max-sm:mb-5 max-sm:w-full sm:mt-3">
+          <SignedIn>
+            {clerkId === userInfo.user.clerkId && (
+              <Link href="/profile/edit">
+                <Button className="paragraph-medium btn-secondary text-dark300_light900 min-h-[46px] min-w-[175px] px-4 py-3">
+                  Edit Profile
+                </Button>
+              </Link>
+            )}
+          </SignedIn>
+        </div>
+      </div>
+    </>
+  );
 };
 
-export default page;
+export default Page;
