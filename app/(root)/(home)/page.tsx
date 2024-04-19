a2521fb745443d9796c92c1337a93d04743904e1
Rakib Islam <rakib71xx@gmail.com>
2024-04-19 18:02:15 +0600

    Pagination Added to the home page

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,23 +1,37 @@
-import Filter from "@/components/Shared/Filter";
-import NoResult from "@/components/Shared/NoResult";
-import LocalSearchBar from "@/components/Shared/search/LocalSearchBar";
 import QuestionCard from "@/components/cards/QuestionCard";
 import HomeFilters from "@/components/home/HomeFilters";
+
 import { Button } from "@/components/ui/button";
 import { HomePageFilters } from "@/constants/filters";
 import { getQuestions } from "@/lib/actions/question.action";
 import { SearchParamsProps } from "@/types";
 import Link from "next/link";
 
+import Filter from "@/components/Shared/Filter";
+import NoResult from "@/components/Shared/NoResult";
+import Pagination from "@/components/Shared/Pagination";
+import LocalSearchbar from "@/components/Shared/search/LocalSearchBar";
+import { auth } from "@clerk/nextjs";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Home | Dev Overflow",
+};
+
 export default async function Home({ searchParams }: SearchParamsProps) {
-  const result = await getQuestions({ searchQuery: searchParams.q });
+  const { userId } = auth();
+
+  const result = await getQuestions({
+    searchQuery: searchParams.q,
+    filter: searchParams.filter,
+    page: searchParams.page ? +searchParams.page : 1,
+  });
+
   return (
     <>
-      <div
-        className="flex w-full flex-col-reverse justify-between gap-4
-            sm:flex-row  sm:items-center"
-      >
+      <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
         <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+
         <Link href="/ask-question" className="flex justify-end max-sm:w-full">
           <Button className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900">
             Ask a Question
@@ -25,20 +39,22 @@ export default async function Home({ searchParams }: SearchParamsProps) {
         </Link>
       </div>
 
-      <div className="mt-11 flex justify-center gap-5 max-sm:flex-col sm:items-center">
-        <LocalSearchBar
+      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+        <LocalSearchbar
           route="/"
           iconPosition="left"
           imgSrc="/assets/icons/search.svg"
           placeholder="Search for questions"
           otherClasses="flex-1"
         />
+
         <Filter
           filters={HomePageFilters}
           otherClasses="min-h-[56px] sm:min-w-[170px]"
           containerClasses="hidden max-md:flex"
         />
       </div>
+
       <HomeFilters />
 
       <div className="mt-10 flex w-full flex-col gap-6">
@@ -58,12 +74,18 @@ export default async function Home({ searchParams }: SearchParamsProps) {
           ))
         ) : (
           <NoResult
-            title="There's No Question to Show"
-            description="sdfhkjsdh hfsdkhf sjd jsfhsjkdfhskjd sdhfkjsdhf sd hksdhf khsdk"
+            title="There’s no question to show"
+            description="Be the first to break the silence! 🚀 Ask a Question and kickstart the discussion. our query could be the next big thing others learn from. Get involved! 💡"
             link="/ask-question"
             linkTitle="Ask a Question"
           />
         )}
+      </div>
+      <div className="mt-10">
+        <Pagination
+          pageNumber={searchParams?.page ? +searchParams.page : 1}
+          isNext={result.isNext}
+        />
       </div>
     </>
   );
