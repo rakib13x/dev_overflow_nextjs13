f2a889c7e9afc2d7ebe9911e20835eb2389a303f
Rakib Islam <rakib71xx@gmail.com>
2024-04-19 08:19:50 +0600

    AnswersTab Component design added

--- components/Shared/AnswersTab.tsx
+++ components/Shared/AnswersTab.tsx
@@ -1,18 +1,33 @@
 import { getUserAnswers } from "@/lib/actions/user.action";
 import { SearchParamsProps } from "@/types";
+import AnswerCard from "../cards/AnswerCard";
 
 interface Props extends SearchParamsProps {
   userId: string;
   clerkId?: string | null;
 }
 
-const AnswersTab = async ({ userId }: Props) => {
+const AnswersTab = async ({ userId, clerkId }: Props) => {
   const result = await getUserAnswers({
     userId,
   });
 
   console.log(result);
-  return <div>AnswersTab</div>;
+  return (
+    <>
+      {result.answers.map((item) => (
+        <AnswerCard
+          key={item._id}
+          clerkId={clerkId}
+          _id={item._id}
+          question={item.question}
+          author={item.author}
+          upvotes={item.upvotes.length}
+          createdAt={item.createdAt}
+        />
+      ))}
+    </>
+  );
 };
 
 export default AnswersTab;
