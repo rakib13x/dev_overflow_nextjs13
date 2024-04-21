851d9c0060d7f40f8c1cbd3d432b93bf0c7ab62e
Rakib Islam <rakib71xx@gmail.com>
2024-04-21 10:20:38 +0600

    Fixed Mobile nav

--- components/Shared/Navbar/MobileNav.tsx
+++ components/Shared/Navbar/MobileNav.tsx
@@ -1,26 +1,28 @@
 "use client";
-import React from "react";
+
+import { Button } from "@/components/ui/button";
 import {
   Sheet,
-  SheetContent,
   SheetClose,
+  SheetContent,
   SheetTrigger,
 } from "@/components/ui/sheet";
+import { sidebarLinks } from "@/constants";
+import { SignedOut } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
-import { SignedOut } from "@clerk/nextjs";
-import { Button } from "@/components/ui/button";
-import { sidebarLinks } from "@/constants";
 import { usePathname } from "next/navigation";
 
 const NavContent = () => {
   const pathname = usePathname();
+
   return (
     <section className="flex h-full flex-col gap-6 pt-16">
       {sidebarLinks.map((item) => {
         const isActive =
           (pathname.includes(item.route) && item.route.length > 1) ||
           pathname === item.route;
+
         return (
           <SheetClose asChild key={item.route}>
             <Link
@@ -70,42 +72,38 @@ const MobileNav = () => {
             src="/assets/images/site-logo.svg"
             width={23}
             height={23}
-            alt="DevOverFlow"
+            alt="DevFlow"
           />
 
-          <p className="h2-bold text-dark100_light900 font-spaceGrotesk  dark:text-light-900">
-            Dev <span className="text-primary-500">OverFlow</span>
+          <p className="h2-bold text-dark100_light900 font-spaceGrotesk">
+            Dev <span className="text-primary-500">Overflow</span>
           </p>
         </Link>
-        <SheetClose asChild>
-          <NavContent />
-        </SheetClose>
-        <SignedOut>
-          <div className="flex flex-col gap-3">
-            <SheetClose asChild>
-              <Link href="/sign-in">
-                <Button
-                  className="small-medium btn-secondary min-h-[41px] w-full 
-                  rounded-lg px-4 py-3 shadow-none
-                "
-                >
-                  <span className="primary-text-gradient">Log In</span>
-                </Button>
-              </Link>
-            </SheetClose>
-            <SheetClose asChild>
-              <Link href="/sign-up">
-                <Button
-                  className="small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] 
-                  w-full rounded-lg px-4 py-3 shadow-none
-                "
-                >
-                  Sign Up
-                </Button>
-              </Link>
-            </SheetClose>
-          </div>
-        </SignedOut>
+        <div>
+          <SheetClose asChild>
+            <NavContent />
+          </SheetClose>
+
+          <SignedOut>
+            <div className="flex flex-col gap-3">
+              <SheetClose asChild>
+                <Link href="/sign-in">
+                  <Button className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
+                    <span className="primary-text-gradient">Log In</span>
+                  </Button>
+                </Link>
+              </SheetClose>
+
+              <SheetClose asChild>
+                <Link href="/sign-up">
+                  <Button className="small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg border px-4 py-3 shadow-none">
+                    Sign Up
+                  </Button>
+                </Link>
+              </SheetClose>
+            </div>
+          </SignedOut>
+        </div>
       </SheetContent>
     </Sheet>
   );
